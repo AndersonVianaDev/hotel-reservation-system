a5a01c8a1dd0b318c5c2eea2540af76e70be49fc
AndersonVianaDev <andersonp.viana.contato@gmail.com>
2024-07-16 17:13:49 -0300

    feat: creating data conflict exception and implementing it

--- src/main/java/com/anderson/hotel_reservation_system/core/customer/usecases/impl/RegisterCustomerUseCaseImpl.java
+++ src/main/java/com/anderson/hotel_reservation_system/core/customer/usecases/impl/RegisterCustomerUseCaseImpl.java
@@ -4,8 +4,10 @@ import com.anderson.hotel_reservation_system.core.customer.dataprovider.Customer
 import com.anderson.hotel_reservation_system.core.customer.domain.Customer;
 import com.anderson.hotel_reservation_system.core.customer.dtos.CustomerDTO;
 import com.anderson.hotel_reservation_system.core.customer.usecases.ports.RegisterCustomerUseCasePort;
+import com.anderson.hotel_reservation_system.core.exceptions.DataConflictException;
 
 import static com.anderson.hotel_reservation_system.core.customer.mapper.CustomerMapper.toCustomer;
+import static com.anderson.hotel_reservation_system.core.exceptions.constants.ExceptionConstants.EMAIL_ALREADY_REGISTERED;
 
 public class RegisterCustomerUseCaseImpl implements RegisterCustomerUseCasePort {
 
@@ -17,7 +19,7 @@ public class RegisterCustomerUseCaseImpl implements RegisterCustomerUseCasePort 
 
     @Override
     public Customer execute(CustomerDTO dto) {
-        if(repository.findByEmail(dto.email()).isPresent()) throw new RuntimeException("email already registered");
+        if(repository.findByEmail(dto.email()).isPresent()) throw new DataConflictException(EMAIL_ALREADY_REGISTERED);
         Customer customer = toCustomer(dto);
         return repository.save(customer);
     }
---
+++ src/main/java/com/anderson/hotel_reservation_system/core/exceptions/constants/ExceptionConstants.java
@@ -0,0 +1,5 @@
+package com.anderson.hotel_reservation_system.core.exceptions.constants;
+
+public class ExceptionConstants {
+    public static String EMAIL_ALREADY_REGISTERED = "email already registered.";
+}
