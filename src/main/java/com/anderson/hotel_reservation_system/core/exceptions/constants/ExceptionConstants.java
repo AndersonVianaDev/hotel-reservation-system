0d3ab0772203cea914b32a7d51b052cd5971aa75
AndersonVianaDev <andersonp.viana.contato@gmail.com>
2024-07-17 14:44:20 -0300

    feat: adding check-in and check-out validation

--- src/main/java/com/anderson/hotel_reservation_system/core/exceptions/constants/ExceptionConstants.java
+++ src/main/java/com/anderson/hotel_reservation_system/core/exceptions/constants/ExceptionConstants.java
@@ -1,10 +1,11 @@
 package com.anderson.hotel_reservation_system.core.exceptions.constants;
 
 public class ExceptionConstants {
-    public static String EMAIL_ALREADY_REGISTERED = "email already registered.";
-    public static String CUSTOMER_NOT_FOUND = "customer not found.";
-    public static String NUMBER_ROOM_ALREADY_REGISTERED = "number room already registered.";
-    public static String ROOM_NOT_FOUND = "room not found.";
-    public static String RESERVATION_NOT_FOUND = "reservation not found.";
-    public static String RESERVATION_STATUS_UNKNOWM = "reservation status unknowm.";
+    public static final String EMAIL_ALREADY_REGISTERED = "email already registered.";
+    public static final String CUSTOMER_NOT_FOUND = "customer not found.";
+    public static final String NUMBER_ROOM_ALREADY_REGISTERED = "number room already registered.";
+    public static final String ROOM_NOT_FOUND = "room not found.";
+    public static final String CHECK_OUT_BEFORE_CHECK_IN = "check-out date must be after check-in date.";
+    public static final String RESERVATION_NOT_FOUND = "reservation not found.";
+    public static final String RESERVATION_STATUS_UNKNOWM = "reservation status unknowm.";
 }
--- src/main/java/com/anderson/hotel_reservation_system/core/reservation/usecases/impl/RegisterReservationUseCaseImpl.java
+++ src/main/java/com/anderson/hotel_reservation_system/core/reservation/usecases/impl/RegisterReservationUseCaseImpl.java
@@ -2,6 +2,7 @@ package com.anderson.hotel_reservation_system.core.reservation.usecases.impl;
 
 import com.anderson.hotel_reservation_system.core.customer.domain.Customer;
 import com.anderson.hotel_reservation_system.core.customer.usecases.ports.FindCustomerByIdUseCasePort;
+import com.anderson.hotel_reservation_system.core.exceptions.InvalidDataException;
 import com.anderson.hotel_reservation_system.core.reservation.dataprovider.ReservationRepository;
 import com.anderson.hotel_reservation_system.core.reservation.domain.Reservation;
 import com.anderson.hotel_reservation_system.core.reservation.dtos.ReservationDTO;
@@ -9,6 +10,7 @@ import com.anderson.hotel_reservation_system.core.reservation.usecases.ports.Reg
 import com.anderson.hotel_reservation_system.core.room.domain.Room;
 import com.anderson.hotel_reservation_system.core.room.usecases.ports.FindRoomByIdUseCasePort;
 
+import static com.anderson.hotel_reservation_system.core.exceptions.constants.ExceptionConstants.CHECK_OUT_BEFORE_CHECK_IN;
 import static com.anderson.hotel_reservation_system.core.reservation.mapper.ReservationMapper.toReservation;
 
 public class RegisterReservationUseCaseImpl implements RegisterReservationUseCasePort {
@@ -27,6 +29,8 @@ public class RegisterReservationUseCaseImpl implements RegisterReservationUseCas
 
     @Override
     public Reservation execute(ReservationDTO dto) {
+        if(!dto.checkIn().isBefore(dto.checkOut())) throw new InvalidDataException(CHECK_OUT_BEFORE_CHECK_IN);
+
         Customer customer = findCustomerById.execute(dto.idCustomer());
         Room room = findRoomById.execute(dto.idRoom());
 
