9d7bd81c049f35e41e73b1ca3894a91c37aae726
AndersonVianaDev <andersonp.viana.contato@gmail.com>
2024-07-17 14:44:54 -0300

    test: creating test for check-in and check-out validation

--- src/test/java/com/anderson/hotel_reservation_system/core/reservation/usecases/impl/RegisterReservationUseCaseImplTest.java
+++ src/test/java/com/anderson/hotel_reservation_system/core/reservation/usecases/impl/RegisterReservationUseCaseImplTest.java
@@ -2,6 +2,7 @@ package com.anderson.hotel_reservation_system.core.reservation.usecases.impl;
 
 import com.anderson.hotel_reservation_system.core.customer.domain.Customer;
 import com.anderson.hotel_reservation_system.core.customer.usecases.ports.FindCustomerByIdUseCasePort;
+import com.anderson.hotel_reservation_system.core.exceptions.InvalidDataException;
 import com.anderson.hotel_reservation_system.core.reservation.dataprovider.ReservationRepository;
 import com.anderson.hotel_reservation_system.core.reservation.domain.Reservation;
 import com.anderson.hotel_reservation_system.core.reservation.dtos.ReservationDTO;
@@ -14,7 +15,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 import static com.anderson.hotel_reservation_system.core.customer.builder.CustomerBuilderTest.toCustomer;
+import static com.anderson.hotel_reservation_system.core.exceptions.constants.ExceptionConstants.CHECK_OUT_BEFORE_CHECK_IN;
 import static com.anderson.hotel_reservation_system.core.reservation.builder.ReservationBuilderTest.toReservationDTO;
 import static com.anderson.hotel_reservation_system.core.room.builder.RoomBuilderTest.toRoom;
 import static org.junit.jupiter.api.Assertions.*;
@@ -53,5 +58,18 @@ class RegisterReservationUseCaseImplTest {
         assertEquals(reservationDTO.checkOut(), reservation.getCheckOut());
         assertEquals(customer, reservation.getCustomer());
         assertEquals(room, reservation.getRoom());
+    }
+
+    @Test
+    @DisplayName("check-out date must be after check-in date")
+    void executeWithInvalidData() {
+        ReservationDTO reservationDTO = new ReservationDTO(UUID.randomUUID(),
+                UUID.randomUUID(),
+                LocalDate.of(2024,7,20),
+                LocalDate.of(2024, 7, 17));
+
+        InvalidDataException exception = assertThrows(InvalidDataException.class, () -> useCase.execute(reservationDTO));
+
+        assertEquals(CHECK_OUT_BEFORE_CHECK_IN, exception.getMessage());
     }
 }
