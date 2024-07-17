16d54e66b4b3021f08702a8cd914cfe64cbb66fb
AndersonVianaDev <andersonp.viana.contato@gmail.com>
2024-07-17 16:15:07 -0300

    test: creating tests for use case to find all reservations by date range

--- src/test/java/com/anderson/hotel_reservation_system/core/reservation/builder/ReservationBuilderTest.java
+++ src/test/java/com/anderson/hotel_reservation_system/core/reservation/builder/ReservationBuilderTest.java
@@ -1,10 +1,12 @@
 package com.anderson.hotel_reservation_system.core.reservation.builder;
 
 import com.anderson.hotel_reservation_system.core.reservation.domain.Reservation;
+import com.anderson.hotel_reservation_system.core.reservation.dtos.DateRangeDTO;
 import com.anderson.hotel_reservation_system.core.reservation.dtos.ReservationDTO;
 import com.anderson.hotel_reservation_system.core.reservation.enums.ReservationStatus;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 import static com.anderson.hotel_reservation_system.core.customer.builder.CustomerBuilderTest.toCustomer;
@@ -27,5 +29,28 @@ public class ReservationBuilderTest {
                 .withCustomer(toCustomer())
                 .withRoom(toRoom())
                 .build();
+    }
+
+    public static DateRangeDTO toDateRangeDTO() {
+        return new DateRangeDTO(LocalDate.of(2024, 7, 17), LocalDate.of(2024, 7, 20));
+    }
+
+    public static List<Reservation> toReservationListSize2() {
+        return List.of( new ReservationBuilder()
+                .withId(UUID.randomUUID())
+                .withCheckIn(LocalDate.of(2024, 7, 17))
+                .withCheckOut(LocalDate.of(2024, 7, 20))
+                .withStatus(ReservationStatus.SCHEDULED)
+                .withCustomer(toCustomer())
+                .withRoom(toRoom())
+                .build(),
+                new ReservationBuilder()
+                        .withId(UUID.randomUUID())
+                        .withCheckIn(LocalDate.of(2024, 7, 17))
+                        .withCheckOut(LocalDate.of(2024, 7, 20))
+                        .withStatus(ReservationStatus.SCHEDULED)
+                        .withCustomer(toCustomer())
+                        .withRoom(toRoom())
+                        .build());
     }
 }
---
+++ src/test/java/com/anderson/hotel_reservation_system/core/reservation/usecases/impl/FindAllReservationsByDateRangeUseCaseImplTest.java
@@ -0,0 +1,55 @@
+package com.anderson.hotel_reservation_system.core.reservation.usecases.impl;
+
+import com.anderson.hotel_reservation_system.core.exceptions.InvalidDataException;
+import com.anderson.hotel_reservation_system.core.reservation.dataprovider.ReservationRepository;
+import com.anderson.hotel_reservation_system.core.reservation.domain.Reservation;
+import com.anderson.hotel_reservation_system.core.reservation.dtos.DateRangeDTO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static com.anderson.hotel_reservation_system.core.exceptions.constants.ExceptionConstants.CHECK_OUT_BEFORE_CHECK_IN;
+import static com.anderson.hotel_reservation_system.core.reservation.builder.ReservationBuilderTest.toDateRangeDTO;
+import static com.anderson.hotel_reservation_system.core.reservation.builder.ReservationBuilderTest.toReservationListSize2;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class FindAllReservationsByDateRangeUseCaseImplTest {
+
+    @InjectMocks
+    private FindAllReservationsByDateRangeUseCaseImpl useCase;
+
+    @Mock
+    private ReservationRepository repository;
+
+    @Test
+    @DisplayName("find all reservations by date range successfully")
+    void execute() {
+        List<Reservation> reservations = toReservationListSize2();
+        DateRangeDTO dto = toDateRangeDTO();
+
+        when(repository.findAllByDateRange(dto.checkIn(), dto.checkOut())).thenReturn(reservations);
+
+        List<Reservation> reservationsListResul = useCase.execute(dto);
+
+        assertEquals(reservations, reservationsListResul);
+    }
+
+    @Test
+    @DisplayName("check-out date must be after check-in date")
+    void executeWithInvalidData() {
+        DateRangeDTO dto = new DateRangeDTO(LocalDate.of(2024, 7, 17), LocalDate.of(2024, 7, 12));
+
+        InvalidDataException exception = assertThrows(InvalidDataException.class, () -> useCase.execute(dto));
+
+        assertEquals(CHECK_OUT_BEFORE_CHECK_IN, exception.getMessage());
+    }
+}
