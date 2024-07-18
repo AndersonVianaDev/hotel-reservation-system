e12a2da086123cce6186a8c0f6a2c750614bc009
AndersonVianaDev <andersonp.viana.contato@gmail.com>
2024-07-18 13:46:39 -0300

    refactor: refactoring the message

--- src/main/java/com/anderson/hotel_reservation_system/entrypoint/customer/dtos/CustomerRequestDTO.java
+++ src/main/java/com/anderson/hotel_reservation_system/entrypoint/customer/dtos/CustomerRequestDTO.java
@@ -6,5 +6,5 @@ import jakarta.validation.constraints.Size;
 
 public record CustomerRequestDTO(@NotNull(message = "name field cannot be null.") String name,
                                  @Email(message = "email format is not valid.") @NotNull(message = "email field cannot be null.") String email,
-                                 @NotNull(message = "password field cannot be null.") @Size(min = 9, max = 11, message = "the phone must be between 9 to 11 digits.") String phone) {
+                                 @NotNull(message = "phone field cannot be null.") @Size(min = 9, max = 11, message = "the phone must be between 9 to 11 digits.") String phone) {
 }
