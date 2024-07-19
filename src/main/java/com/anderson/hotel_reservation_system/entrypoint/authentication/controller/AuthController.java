f530f8d512307feaacbf1799911e86810288c20d
AndersonVianaDev <andersonp.viana.contato@gmail.com>
2024-07-19 20:37:37 -0300

    feat: creating auth controller

---
+++ src/main/java/com/anderson/hotel_reservation_system/entrypoint/authentication/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.anderson.hotel_reservation_system.entrypoint.authentication.controller;
+
+import com.anderson.hotel_reservation_system.config.token.impl.TokenServiceImpl;
+import com.anderson.hotel_reservation_system.config.token.port.TokenService;
+import com.anderson.hotel_reservation_system.dataprovider.employee.entity.EmployeeEntity;
+import com.anderson.hotel_reservation_system.entrypoint.authentication.dtos.LoginDTO;
+import com.anderson.hotel_reservation_system.entrypoint.authentication.dtos.TokenResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenServiceImpl tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponseDTO> login(@RequestBody LoginDTO dto) {
+        try {
+            var usernamePassword = new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
+            var auth = authenticationManager.authenticate(usernamePassword);
+            String token = tokenService.generator((EmployeeEntity) auth.getPrincipal());
+            return ResponseEntity.ok(new TokenResponseDTO(token));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
---
+++ src/main/java/com/anderson/hotel_reservation_system/entrypoint/authentication/dtos/TokenResponseDTO.java
@@ -0,0 +1,4 @@
+package com.anderson.hotel_reservation_system.entrypoint.authentication.dtos;
+
+public record TokenResponseDTO(String token) {
+}
