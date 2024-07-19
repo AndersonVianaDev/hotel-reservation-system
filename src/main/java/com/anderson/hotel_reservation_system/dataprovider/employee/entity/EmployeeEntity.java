c095a6b5ac61b55b26de5372cd5953f5437f694c
AndersonVianaDev <andersonp.viana.contato@gmail.com>
2024-07-19 15:36:10 -0300

    refactor: adding @Enumerated

--- src/main/java/com/anderson/hotel_reservation_system/dataprovider/employee/entity/EmployeeEntity.java
+++ src/main/java/com/anderson/hotel_reservation_system/dataprovider/employee/entity/EmployeeEntity.java
@@ -14,6 +14,7 @@ public class EmployeeEntity {
     private String name;
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING)
     private EmployeeType type;
 
     private EmployeeEntity() {}
