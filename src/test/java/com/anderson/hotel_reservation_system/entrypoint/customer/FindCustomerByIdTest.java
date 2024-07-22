e71959a578bdfffd07f5ec37b34ecd61e25a6029
AndersonVianaDev <andersonp.viana.contato@gmail.com>
2024-07-22 15:14:32 -0300

    test: creating tests to find customer by id

---
+++ src/test/java/com/anderson/hotel_reservation_system/entrypoint/customer/FindCustomerByIdTest.java
@@ -0,0 +1,64 @@
+package com.anderson.hotel_reservation_system.entrypoint.customer;
+
+import com.anderson.hotel_reservation_system.core.customer.domain.Customer;
+import com.anderson.hotel_reservation_system.dataprovider.customer.repositories.impl.CustomerRepositoryImpl;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.UUID;
+
+import static com.anderson.hotel_reservation_system.core.customer.builder.CustomerBuilderTest.toCustomer;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+public class FindCustomerByIdTest {
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private CustomerRepositoryImpl repository;
+
+    @Test
+    @DisplayName("find customer by id successfully")
+    void findByIdSuccessfully() throws Exception {
+        Customer customer = toCustomer();
+        Customer savedCustomer = repository.save(customer);
+        UUID id = savedCustomer.getId();
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/customer/get/" + id))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+
+        Customer customerResult = mapper.readValue(content, new TypeReference<Customer>() {});
+
+        assertEquals(savedCustomer, customerResult);
+    }
+
+    @Test
+    @DisplayName("Not found exception")
+    void findByIdWithNotFound() throws Exception {
+        UUID id = UUID.randomUUID();
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/customer/get/" + id))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andDo(MockMvcResultHandlers.print());
+    }
+}
