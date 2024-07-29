16ee8132890d732c0363cb1672fdd8f00417bf70
AndersonVianaDev <andersonp.viana.contato@gmail.com>
2024-07-29 17:29:26 -0300

    feat: creating employee admin

---
+++ src/main/resources/db/migration/V5__insert_admin.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_employees (id, name, email, password, type)
+VALUES (
+    'f98bd201-27de-4302-b473-e3ebd3da98be',
+    'admin',
+    'admin@gmail.com',
+    '$2a$10$fDwJbrEm0Bo.xyLtCW4UaO2ZjJzrWBnXT5PyM7JWxgUo3pzQANXHe',
+    'ADMIN'
+);
