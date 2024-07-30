e199e4bd5cf93c9bf228d7c8f28e490b6fb679bb
AndersonVianaDev <andersonp.viana.contato@gmail.com>
2024-07-30 14:10:12 -0300

    feat: adding jwat token in swagger

--- src/main/java/com/anderson/hotel_reservation_system/HotelReservationSystemApplication.java
+++ src/main/java/com/anderson/hotel_reservation_system/HotelReservationSystemApplication.java
@@ -1,7 +1,10 @@
 package com.anderson.hotel_reservation_system;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +12,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @OpenAPIDefinition(info = @Info(
 		title = "hotel-reservation-system",
 		version = "1",
-		description = "Hotel reservation management system. This service allows for the administration of customers, rooms, and reservations, offering functionalities such as registering new customers and employees, managing room availability, and tracking reservation statuses.")
+		description = "Hotel reservation management system. This service allows for the administration of customers, rooms, and reservations, offering functionalities such as registering new customers and employees, managing room availability, and tracking reservation statuses."),
+		security = @SecurityRequirement(name = "Authorization")
+)
+@SecurityScheme(
+		name = "Authorization",
+		type = SecuritySchemeType.HTTP,
+		scheme = "bearer",
+		bearerFormat = "JWT"
 )
 public class HotelReservationSystemApplication {
 
