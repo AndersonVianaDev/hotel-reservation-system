7d1aa70f34cb9f671b9910ab31f2a151c784ddc9
Anderson Palmerim <andersonp.viana.contato@gmail.com>
2024-08-10 11:55:38 -0300

    test: update dates in reservation registration tests

--- src/test/java/com/anderson/hotel_reservation_system/entrypoint/reservation/builders/ReservationBuilderTest.java
+++ src/test/java/com/anderson/hotel_reservation_system/entrypoint/reservation/builders/ReservationBuilderTest.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 
 public class ReservationBuilderTest {
     public static ReservationRequestDTO toReservationRequestDTO() {
-        return new ReservationRequestDTO(LocalDate.of(2024, 7, 24), LocalDate.of(2024, 7, 28));
+        return new ReservationRequestDTO(LocalDate.of(2030, 7, 24), LocalDate.of(2030, 7, 28));
     }
 
     public static ReservationRequestDTO toReservationRequestDTOWithInvalidData() {
